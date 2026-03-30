18453956a82d1e735dd9697ca1cd243d2e53b745
jjennasin <jennasin@ufl.edu>
2026-03-30 15:12:41 -0400

    moved api keys...

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -6,14 +6,15 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBAcHodjnoT0F1lGDk2ht57T-CcLYFEVhM",
-  authDomain: "pick-easy-c46ba.firebaseapp.com",
-  projectId: "pick-easy-c46ba",
-  storageBucket: "pick-easy-c46ba.firebasestorage.app",
-  messagingSenderId: "553003948498",
-  appId: "1:553003948498:web:56508a391930e27641900b",
-  measurementId: "G-CKCVCFC4Z5"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
